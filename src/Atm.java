df7c498ac9a010d0a64a2de6dcc191c93450fe71
arthur andrade <arthurandrad.23@gmail.com>
2024-02-25 01:13:41 -0300

    Working in user login

--- src/Atm.java
+++ src/Atm.java
@@ -13,48 +13,49 @@ public class Atm {
         System.out.println("Hello, do you already have an account? \n1.Yes\n2.No");
         Integer userInput= read.nextInt();
 
-        if (userInput == 1) {
-            userHasAccount = true;
-
-        } else if (userInput == 2) {
-            userHasAccount = false;
-
-            System.out.println("Would you like to create an account? \n1.Yes\n2.No");
-            userInput= read.nextInt();
-
-            if (userInput == 1) {
+        switch (userInput) {
+            case 2:            
                 NUM_ACCOUNTS++;
-
                 BankAccount u = new BankAccount();
-    
+
                 System.out.println("Please state your name");
                 String name = read.next();
-    
+
                 System.out.println("Please enter your password (Only numbers)");
                 Integer password = read.nextInt();
-    
+
                 System.out.println("Please deposit any amount of money");
                 Float money = read.nextFloat();
-    
+
                 u.setName(name);
                 u.setPassword(password);
                 u.setoney(money);    
-
                 bankAccounts.add(u);
                 read.nextLine();
 
-                System.out.println("Current user: \n" + u); 
+                System.out.println("Current user: \n" + u);
 
-            } else if (userInput == 2) {
-                System.out.println("Goodbye!");
-            } else {
-                System.out.println("Please enter a valid option \n1.Yes\n2.No");
+                userHasAccount = true ;
+
+                System.out.println("Please state your name");
+                String nameLogin = read.next();
+                System.out.println("Please enter your password (Only numbers)");
+                Integer passwordLogin = read.nextInt();
+
+                u.getName();
+
+                if (u.getName().equals(nameLogin) && u.getPassword() == (passwordLogin)) {
+                    System.out.println("Login successful");
+                }
+
+            case 1:
+                
+            break;
+        
+            default:
+                System.out.println("Please enter a valid option \n1. Yes\n2.No");
                 userInput= read.nextInt();
-            }
-
-        } else {
-            System.out.println("Please enter a valid option \n1. Yes\n2.No");
-            userInput= read.nextInt();
+            break;
         }
 
         read.close();
